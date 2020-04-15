cd4c210ca7b5519a0e0d962f8cc31cd4218b56da
rirze <cheedella.sourabh@gmail.com>
2020-04-15 16:02:24 -0400

    update docs

--- mirobot/__init__.py
+++ mirobot/__init__.py
@@ -1,3 +1,10 @@
 from .mirobot import Mirobot
 
+# don't document our resources directory duh
+__pdoc__ = {}
+__pdoc__['resources'] = False
+__pdoc__['resources.__init__'] = False
+
+
+# if someone imports by '*' then import everything in mirobot
 __all__ = ['mirobot']
