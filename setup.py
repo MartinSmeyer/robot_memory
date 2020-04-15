90cd1250b7852fa24c4c21f027cded84efb0bd55
rirze <cheedella.sourabh@gmail.com>
2020-04-15 14:48:20 -0400

    add more pypi classifiers

--- setup.py
+++ setup.py
@@ -12,11 +12,33 @@ setuptools.setup(name='mirobot-py',
                  long_description_content_type='text/markdown',
                  url="https://github.com/rirze/mirobot-py",
                  packages=['mirobot'],
-                 classifiers=[
-                     "Programming Language :: Python :: 3",
-                     "License :: OSI Approved :: MIT License",
-                     "Operating System :: OS Independent",
-                 ],
+                 classifiers="""
+                 Development Status :: 4 - Beta
+                 Programming Language :: Python :: 3 :: Only
+                 Programming Language :: Python :: 3.6
+                 Programming Language :: Python :: 3.7
+                 Programming Language :: Python :: 3.8
+                 License :: OSI Approved :: MIT License
+                 Operating System :: OS Independent
+                 Operating System :: Microsoft :: Windows
+                 Operating System :: POSIX
+                 Operating System :: Unix
+                 Operating System :: MacOS
+                 Topic :: Scientific/Engineering
+                 Topic :: Education
+                 Topic :: Documentation
+                 Topic :: Home Automation
+                 Topic :: Scientific/Engineering :: Artificial Intelligence
+                 Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)
+                 Topic :: Scientific/Engineering :: Image Recognition
+                 Topic :: Software Development :: Embedded Systems
+                 Topic :: Software Development :: Version Control :: Git
+                 Topic :: Terminals :: Serial
+                 Intended Audience :: Education
+                 Intended Audience :: Science/Research
+                 Intended Audience :: Manufacturing
+                 Intended Audience :: Developers
+                 """.splitlines(),
                  python_requires='>=3.6',
                  install_requires=open('requirements.txt', 'r').read().splitlines(),
                  package_dir={'mirobot': 'mirobot'},
