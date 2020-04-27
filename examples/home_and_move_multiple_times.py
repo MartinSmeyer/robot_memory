67dadde3ce7cf06808514ccff96073d827e9fb91
rirze <cheedella.sourabh@gmail.com>
2020-04-27 12:40:54 -0400

    update home_and_move example to use new aliases

--- examples/home_and_move_multiple_times.py
+++ examples/home_and_move_multiple_times.py
@@ -4,11 +4,11 @@ from mirobot import Mirobot
 # Default for `wait=` is `True`, but explicitly state it here to showcase this.
 with Mirobot(wait=True) as m:
 
-    # Mirobot will by default wait for any command because we specified `wait=True` above.
+    # Mirobot will by default wait for any command because we specified `wait=True` for the class above.
     m.home_simultaneous()
 
     # print our dataclass maintained by Mirobot. Shows the x,y,z,a,b,c coordinates.
-    print(m.status.cartesian)
+    print(m.cartesian)
 
     # increment arm's position using a for-loop
     for count in range(5):
@@ -16,10 +16,11 @@ with Mirobot(wait=True) as m:
         my = 0.00 + count * 5
         mz = 170 + count * 5
 
-        print(f"************Count {count}**********")
+        print(f"************Count {count}************")
 
         # notice how we don't need any "wait" or "sleep" commands!
         # the following command will only return when the Mirobot returns 'Ok' and when Mirobot is 'Idle'
         m.go_to_cartesian_ptp(mx, my, mz)
 
-        print(m.status.cartesian)
+        # print our cartesian coordinates again
+        print(m.cartesian)
