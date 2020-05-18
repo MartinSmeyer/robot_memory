cd5c457aa6546c848b55d85b830bf048853a85a2
rirze <cheedella.sourabh@gmail.com>
2020-05-18 14:47:58 -0400

    Use GitHub Actions to publish releases to both GitHub and PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 
 setuptools.setup(name='mirobot-py',
-                 version='1.0.43',
+                 version='1.0.52',
                  description="A Python interface library for WKlata's Mirobot",
                  author='Sourabh Cheedella',
                  author_email='cheedella.sourabh@gmail.com',
