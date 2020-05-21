6d25d9ac8df252e2c3d6cb3605fa21228487c87c
rirze <cheedella.sourabh@gmail.com>
2020-05-21 18:06:56 -0400

    fix rover commands from being discrete to continous toggles

--- mirobot/base_rover.py
+++ mirobot/base_rover.py
@@ -1,15 +1,14 @@
 import functools
+from time import sleep
 
 
 class BaseRover:
     def __init__(self, mirobot):
-        "docstring"
         self._mirobot = mirobot
 
-    def repeat_decorator(fn):
-
+    def time_decorator(self, fn):
         @functools.wraps(fn)
-        def repeat_wrapper(self, *args, **kwargs):
+        def time_wrapper(self, *args, **kwargs):
             args_names = fn.__code__.co_varnames[:fn.__code__.co_argcount]
             args_dict = dict(zip(args_names, args))
 
@@ -21,62 +20,69 @@ class BaseRover:
                 else:
                     return default
 
-            repeat = get_arg('repeat', 1)
+            time = get_arg('time', 0)
+            wait = get_arg('wait', True)
 
-            output = []
-            for i in range(repeat):
-                output.extend(fn(self, *args, **kwargs))
+            output = fn(self, *args, **kwargs)
+
+            if time:
+                sleep(time)
+                self.stop(wait=wait)
 
             return output
 
-        return repeat_wrapper
+        return time_wrapper
 
-    @repeat_decorator
-    def move_upper_left(self, repeat=1, wait=True):
+    @time_decorator
+    def move_upper_left(self, time=0, wait=True):
         instruction = "W7"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
 
-    @repeat_decorator
-    def move_upper_right(self, repeat=1, wait=True):
+    @time_decorator
+    def move_upper_right(self, time=0, wait=True):
         instruction = "W9"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
 
-    @repeat_decorator
-    def move_bottom_left(self, repeat=1, wait=True):
+    @time_decorator
+    def move_bottom_left(self, time=0, wait=True):
         instruction = "W1"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
 
-    @repeat_decorator
-    def move_bottom_right(self, repeat=1, wait=True):
+    @time_decorator
+    def move_bottom_right(self, time=0, wait=True):
         instruction = "W3"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
 
-    @repeat_decorator
-    def move_forward(self, repeat=1, wait=True):
-        instruction = "W8"
-        return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
-
-    @repeat_decorator
-    def move_backward(self, repeat=1, wait=True):
-        instruction = "W2"
-        return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
-
-    @repeat_decorator
-    def move_left(self, repeat=1, wait=True):
+    @time_decorator
+    def move_left(self, time=0, wait=True):
         instruction = "W4"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
 
-    @repeat_decorator
-    def move_right(self, repeat=1, wait=True):
+    @time_decorator
+    def move_right(self, time=0, wait=True):
         instruction = "W6"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
 
-    @repeat_decorator
-    def rotate_left(self, repeat=1, wait=True):
+    @time_decorator
+    def rotate_left(self, time=0, wait=True):
         instruction = "W10"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
 
-    @repeat_decorator
-    def rotate_right(self, repeat=1, wait=True):
+    @time_decorator
+    def rotate_right(self, time=0, wait=True):
         instruction = "W11"
+        return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
+
+    @time_decorator
+    def move_forward(self, time=0, wait=True):
+        instruction = "W8"
+        return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
+
+    @time_decorator
+    def move_backward(self, time=0, wait=True):
+        instruction = "W2"
+        return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
+
+    def stop(self, wait=True):
+        instruction = "W0"
         return self._mirobot.send_msg(instruction, wait=wait, terminator='\r\n')
